dcaee92cf7cad41c0e0375cc3caebda65662d18d
Sardar-Taha <tahaabb1122@gmail.com>
2022-12-01 21:35:52 +0500

    first commit

---
+++ Switch/index.js
@@ -0,0 +1,21 @@
+let day = prompt("text here")
+
+switch(day) {
+    case "monday" :
+        alert("yup")
+    break
+        case "Tuseday" :
+            alert("yu")
+            break
+
+            case "Wednesday" :
+                alert("yup")
+                break
+
+                case "Thursday" :
+                    alert("yup")
+                    break
+
+                    case "Friday" :
+                        alert("yup")
+}
---
+++ for loop/index.js
@@ -0,0 +1,32 @@
+// let x 
+// console.log(x,typeof x);
+
+// let x = "8" //string
+// let y =  1  // number
+
+// let result = x + y
+// console.log(result);
+
+// let x = 2
+// let y = 2
+//                   // boolean
+// let result = x >= y
+// console.log(result);
+
+
+//what happen if i add empty string into a number ?
+
+// let x = 8 
+// let y = ""
+
+// let result = x + y
+// console.log(result);
+
+
+//i want number between number and string 
+
+let x = parseInt("123 taha")
+
+console.log(x);
+
+//that how you can number :) 
